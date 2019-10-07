25bf9034a60d53e41c83f2ba502c384f02e82be0
Duncan Macleod <duncan.macleod@ligo.org>
2019-10-07 19:41:57 +0100

    add activate/deactivate scripts
    to set LALSIMULATION_DATADIR

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -e
 # only link libraries we actually use
 export GSL_LIBS="-L${PREFIX}/lib -lgsl"
 
+# configure
 ./configure \
 	--prefix="${PREFIX}" \
 	--disable-doxygen \
@@ -15,6 +16,36 @@ export GSL_LIBS="-L${PREFIX}/lib -lgsl"
 	--disable-python \
 	--disable-gcc-flags \
 	--enable-silent-rules
+
+# build
 make -j ${CPU_COUNT}
+
+# check
 make -j ${CPU_COUNT} check
+
+# install
 make install
+
+# -- create activate/deactivate scripts
+PKG_NAME_UPPER=$(echo ${PKG_NAME} | awk '{ print toupper($0) }')
+
+# activate.sh
+ACTIVATE_SH="${PREFIX}/etc/conda/activate.d/activate_${PKG_NAME}.sh"
+mkdir -p $(dirname ${ACTIVATE_SH})
+cat > ${ACTIVATE_SH} << EOF
+#!/bin/bash
+export CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR="\${${PKG_NAME_UPPER}_DATADIR:-empty}"
+export ${PKG_NAME_UPPER}_DATADIR="/opt/anaconda1anaconda2anaconda3/share/${PKG_NAME}"
+EOF
+# deactivate.sh
+DEACTIVATE_SH="${PREFIX}/etc/conda/deactivate.d/deactivate_${PKG_NAME}.sh"
+mkdir -p $(dirname ${DEACTIVATE_SH})
+cat > ${DEACTIVATE_SH} << EOF
+#!/bin/bash
+if [ "\${CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR}" == "empty" ]; then
+	unset ${PKG_NAME_UPPER}_DATADIR
+else
+	export ${PKG_NAME_UPPER}_DATADIR="\${CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR}"
+fi
+unset CONDA_BACKUP_${PKG_NAME_UPPER}_DATADIR
+EOF
